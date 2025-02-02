132765e30390d58e4622fbbd769f44f2cae4a714
Pasindu Dilshan <dilshanp5400@gmail.com>
2025-02-02 14:03:28 +0530

    Added password field toggle functionality.

--- FE/src/pages/Login/Login.jsx
+++ FE/src/pages/Login/Login.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 function Login() {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
   return (
     <>
       <Navbar />
@@ -12,6 +16,30 @@ function Login() {
             <h2 className="mb-5 text-2xl">Login Here</h2>
 
             <input type="text" placeholder="Email" className="input-box" />
+
+            <div className="">
+              <input
+                placeholder="Password"
+                className="input-box"
+                type={isShowPassword ? "text" : "password"}
+              />
+              <div className="flex justify-end relative top-[-50px] right-2">
+                {isShowPassword ? (
+                  <FaEyeSlash
+                    size={20}
+                    className="cursor-pointer "
+                    onClick={() => setIsShowPassword(!isShowPassword)}
+                  />
+                ) : (
+                  <FaEye
+                    size={20}
+                    className="cursor-pointer "
+                    onClick={() => setIsShowPassword(!isShowPassword)}
+                  />
+                )}
+              </div>
+            </div>
+
             <button type="submit" className="btn-primary">
               Login
             </button>
